6e9f595a80528169d73506c1288cb9b6a3436a60
bart_mf <bartcsc@yandex.ru>
2022-05-05 16:06:17 +0300

    exchange "if" on "switch", and check default case.

--- addressbook-web-tests/src/test/java/ru/bart/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/bart/addressbook/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
@@ -23,12 +24,17 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if(browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.EDGE)) {
-            wd = new EdgeDriver();
+        switch (browser) {
+            case BrowserType.CHROME:
+                wd = new ChromeDriver();
+                break;
+            case BrowserType.EDGE:
+                wd = new EdgeDriver();
+                break;
+            case BrowserType.IE:
+                wd = new InternetExplorerDriver();
+                break;
+            default: wd = new FirefoxDriver();
         }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/bart/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/bart/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.bart.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.bart.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager applicationManager = new ApplicationManager("firefox");
+    protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
