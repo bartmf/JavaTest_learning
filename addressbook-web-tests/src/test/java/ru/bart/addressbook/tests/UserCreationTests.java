e8b1c5ae1936131ee6e8a1477d1562f0d8e707f9
bart_mf <bartcsc@yandex.ru>
2022-05-12 12:08:22 +0300

    Task 13

--- addressbook-web-tests/src/test/java/ru/bart/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/bart/addressbook/tests/UserCreationTests.java
@@ -1,10 +1,18 @@
 package ru.bart.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.bart.addressbook.model.UserData;
 import ru.bart.addressbook.model.Users;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -12,17 +20,30 @@ import static org.testng.Assert.*;
 
 public class UserCreationTests extends TestBase{
 
-  @Test
-  public void testUserCreationTests() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validUsers() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/java/ru/bart/addressbook/resources/users.json"));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json+= line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<UserData> list = gson.fromJson(json, new TypeToken<List<UserData>>(){}.getType());
+    return list.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validUsers")
+  public void testUserCreationTests(UserData user){
     app.goTo().homePage();
     Users before = app.user().all();
     app.goTo().createNewUser();
-    File photo = new File("src/test/java/ru/bart/addressbook/resources/photo.jpg");
-    UserData user = new UserData().withName("lolo").withLastName("popo").withPhoto(photo);
     app.user().create(user);
     app.goTo().homePage();
     assertEquals(app.user().count(), before.size() + 1);
     Users after = app.user().all();
-    assertThat(after, equalToObject(before.withAdded(user)));
+    assertThat(after, equalToObject(before.withAdded(user.withId(after.stream()
+            .mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
--- addressbook-web-tests/src/test/java/ru/bart/addressbook/tests/UserModifTests.java
+++ addressbook-web-tests/src/test/java/ru/bart/addressbook/tests/UserModifTests.java
@@ -1,9 +1,19 @@
 package ru.bart.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.bart.addressbook.model.UserData;
 import ru.bart.addressbook.model.Users;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -19,12 +29,26 @@ public class UserModifTests extends TestBase{
                     withName("TestNotModifName").withLastName("TestNotModifLastName"));
         }
     }
-    @Test
-    void editUser() {
+
+    @DataProvider
+    public Iterator<Object[]> validUsers() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/java/ru/bart/addressbook/resources/users.json"));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json+= line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<UserData> list = gson.fromJson(json, new TypeToken<List<UserData>>(){}.getType());
+        return list.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validUsers")
+    void editUser(UserData user) {
         Users before = app.user().all();
         UserData modUser = before.iterator().next();
-        UserData user = new UserData().withId(modUser.getId()).withName("nameModif").withLastName("LastName Modif");
-        app.user().edit(user);
+        app.user().edit(user.withId(modUser.getId()));
         assertEquals(app.user().count(), before.size());
         Users after = app.user().all();
         assertThat(after, equalToObject(before.without(modUser).withAdded(user)));
