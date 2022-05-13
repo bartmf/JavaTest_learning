d5a505f64df53a0f84e86f80f7e7bde01415a1e2
bart_mf <bartcsc@yandex.ru>
2022-05-13 21:12:38 +0300

    Rest lection

---
+++ rest-sample/src/test/java/ru/bart/rest/RestTest.java
@@ -0,0 +1,46 @@
+package ru.bart.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class RestTest {
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+    }
+
+
+    private Set<Issue> getIssues() throws IOException {
+        String json = getExecuter().execute(Request.Get("https://bugify.stqa.ru/api/issues.json")).returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
+    }
+
+    private Executor getExecuter() {
+        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+
+    private int createIssue(Issue newIssue) throws IOException {
+        String json = getExecuter().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
+                        .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                                new BasicNameValuePair("description", newIssue.getDescription())))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+}
